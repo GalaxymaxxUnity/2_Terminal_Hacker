209c7ba753f4941e81ab06cba432d2777e419a05
Ruan Xun <galaxymaxx@outlook.com>
2018-02-11 22:43:07 -0500

    Finish guessing anagram game

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,30 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.UIElements.GraphView;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+	// Game configuration
+	private string[] level1Password = {"addition", "drawing", "pencil", "blackboard", "eraser"};
+	private string[] level2Password = {"geology", "metaphor", "Napoleon", "quadratic", "cardio"};
+	private string[] level3Password = {"renaissence", "calculus", "template", "algorithm", "artifical intelligence"};
+	
 	// Game state
-	int level;
-	enum Screen
-	{
+	private int _level;
+	private enum Screen {
 		MainMenu,
 		Password,
 		Win
 	}
-	Screen _currentScreen = Screen.MainMenu;
+
+	private Screen _currentScreen;
+	private string _password;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		ShowMainMenu();
 	}
 
-	void ShowMainMenu()
-	{
+	private void ShowMainMenu() {
+		_currentScreen = Screen.MainMenu;
 		Terminal.ClearScreen();
 		Terminal.WriteLine("Hello Ben");
-        		Terminal.WriteLine("What would you like to hack into?\n\nPress 1 for the teacher's classroon\nPress 2 for Professor's office\nPress 3 for the principal's guesthouse\n\nEnter your selection");
+        Terminal.WriteLine("What would you like to hack into?\n\nPress 1 for the teacher's classroon\nPress 2 for Professor's office\nPress 3 for the principal's guesthouse\n\nEnter your selection");
 	}
 
 	
@@ -35,56 +37,111 @@ public class Hacker : MonoBehaviour {
 			_currentScreen = Screen.MainMenu;
 		}
 
-		if (_currentScreen == Screen.MainMenu){
+		else if (_currentScreen == Screen.MainMenu){
 			RunMainMenu(input);
 		}
 
-		if (_currentScreen == Screen.Password) {
-			GuessPassword(input);
+		else if (_currentScreen == Screen.Password) {
+			CheckPassword(input);
+		}
+	}
+	void RunMainMenu(string input) {
+		bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+		if (isValidLevelNumber) {
+			_level = int.Parse(input);
+			AskForPassword();
+		} else {
+			switch (input) {
+				case "007":	// easter egg
+					Terminal.WriteLine("Please select a level, Mr. Bond");
+					break;
+				default:
+					Debug.LogError("Invalid level number");
+					break;
+			}
+		}
+		}
+
+	void AskForPassword() {
+		print(level1Password.Length);
+		_currentScreen = Screen.Password;
+		Terminal.ClearScreen();
+		SetRandomPassword();
+		Terminal.WriteLine("Please enter your password hint : "+_password.Anagram());
+	}
+
+	private void SetRandomPassword() {
+		switch (_level) {
+			case 1:
+				_password = level1Password[Random.Range(0, level1Password.Length)];
+				break;
+			case 2:
+				_password = level2Password[Random.Range(0, level2Password.Length)];
+				break;
+			case 3:
+				_password = level3Password[Random.Range(0, level3Password.Length)];
+				break;
 		}
 	}
 
-	void GuessPassword(string input) {
-		string[] easy = {"table","pencil",""}
-	}
-
-	void RunMainMenu(string input)
-	{
-		if (input == "007"){
-			Terminal.WriteLine("Please select a level, Mr. Bond");
-		}
-		else if (input == "1")
-		{
-			level = 1;
-			StartGame();
-			_currentScreen = Screen.Password;
-		}
-		else if (input == "2")
-		{
-			level = 2;
-			StartGame();
-			_currentScreen = Screen.Password;
-		}
-		else if (input == "3")
-		{
-			level = 3;
-			StartGame();
-			_currentScreen = Screen.Password;
-		}
-		else if (input == "menu")
-		{
-			ShowMainMenu();
-			_currentScreen = Screen.MainMenu;
+	void CheckPassword(string input) {
+		if (input == _password) {
+			DisplayWinScreen();
+		} else {
+			Terminal.WriteLine("Sorry, wrong password");
+			AskForPassword();
 		}
 	}
 
-	void StartGame()
-	{
-		Terminal.WriteLine("You have chosen level "+level);
+	void DisplayWinScreen() {
+		Terminal.ClearScreen();
+		_currentScreen = Screen.Win;
+		Terminal.WriteLine("Congratulations! You successfully dicipher the code.");
+		ShowLevelReward();
 	}
+
+	private void ShowLevelReward() {
+		switch (_level) {
+			case 1:
+				Terminal.WriteLine("Have a book");
+				Terminal.WriteLine(@"
+    ______
+   /    //
+  /    //
+ /____//
+(____(/
+");
+				break;
+			case 2:
+				Terminal.WriteLine("You got a prison key");
+				Terminal.WriteLine(@"
+ __
+/0 \________
+\__/-='-='-
+");
+				break;
+			case 3:
+				Terminal.WriteLine("You can go to Mars now");
+				Terminal.WriteLine(@"
+  /\
+  ||
+  ||
+ /__\
+  ||
+");
+				break;
+			default:
+				Debug.LogError("Invalid level reached");
+				break;
+		}
+	}
+
+	
+
+	
 
 	// Update is called once per frame
-	void Update () { 
+	void Update () {
 		
 	}
 }
